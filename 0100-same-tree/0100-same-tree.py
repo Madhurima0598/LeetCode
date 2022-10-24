bf481dff126342748f934b3f5e69ef16bc00698e
Madhurima Gazula <90411867+Madhurima0598@users.noreply.github.com>
2022-10-24 14:21:37 -0700

    Time: 56 ms (49.83%), Space: 14 MB (29.04%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        #dfs recursive
         if not p and not q:
             return True
         if not p or not q or p.val != q.val:
